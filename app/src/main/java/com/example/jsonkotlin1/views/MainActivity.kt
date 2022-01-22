50b1eb4f7543241d2aa189835a819a28c47bcc2f
Jingyan Sun <jsun2008@163.com>
2022-01-21 21:29:46 -0800

    MVVM

--- app/src/main/java/com/example/jsonkotlin1/views/MainActivity.kt
+++ app/src/main/java/com/example/jsonkotlin1/views/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.example.jsonkotlin1.views
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.lifecycle.ViewModelProvider
 import com.example.jsonkotlin1.R
 import com.example.jsonkotlin1.models.Item
-import com.example.jsonkotlin1.utilities.InjectorUtils
-import com.example.jsonkotlin1.viewmodels.ItemViewModel
-import org.json.JSONArray
-import org.json.JSONTokener
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.concurrent.CountDownLatch
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     // Use items to store the data from the json file
@@ -72,7 +62,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initializeUi()
 
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
@@ -88,15 +77,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun initializeUi(){
-        val factory = InjectorUtils.provideItemsViewModelFactory()
-        val viewModel = ViewModelProvider(this, factory).get(ItemViewModel::class.java)
-
-        /*
-        viewModel.getItems().observe(this, Observer {
-
-        })
-        */
-
-    }
 }
