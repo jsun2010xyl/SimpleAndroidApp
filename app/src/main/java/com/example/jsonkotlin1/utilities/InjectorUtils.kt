e793154b0812c2639f058fd8f60ae765d8f2c62b
Jingyan Sun <jsun2008@163.com>
2022-01-21 16:43:50 -0800

    Update InjectorUtils.kt

--- app/src/main/java/com/example/jsonkotlin1/utilities/InjectorUtils.kt
+++ app/src/main/java/com/example/jsonkotlin1/utilities/InjectorUtils.kt
@@ -1,4 +1,13 @@
 package com.example.jsonkotlin1.utilities
 
+import com.example.jsonkotlin1.models.FakeDatabase
+import com.example.jsonkotlin1.models.ItemRepository
+import com.example.jsonkotlin1.viewmodels.ItemViewModelFactory
+
 object InjectorUtils {
+
+    fun provideItemsViewModelFactory(): ItemViewModelFactory {
+        val itemRepository = ItemRepository.getInstance(FakeDatabase.getInstance().itemDao)
+        return ItemViewModelFactory(itemRepository)
+    }
 }
