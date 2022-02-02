47adb3325837dca6045fab5ae7bf1bc3559e9b1e
Jingyan Sun <jsun2008@163.com>
2022-02-02 01:55:14 -0800

    ViewModel

--- app/src/main/java/com/example/jsonkotlin1/viewmodels/ItemViewModel.kt
+++ app/src/main/java/com/example/jsonkotlin1/viewmodels/ItemViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.jsonkotlin1.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.example.jsonkotlin1.data.ItemRepository
+import com.example.jsonkotlin1.data.repository.ItemListRepository
+import com.example.jsonkotlin1.internal.lazyDeferred
 
 // ViewModel存在的目的就是减少coupling
-class ItemViewModel (private val itemRepository: ItemRepository)
+// class ItemViewModel (private val itemRepository: ItemRepository)
+class ItemViewModel (private val itemListRepository: ItemListRepository)
     : ViewModel() {
 
-    fun getItems() = itemRepository.getItems()
-
+    // fun getItems() = itemRepository.getItems()
+    val itemList by lazyDeferred {
+        itemListRepository.getItemList()
+    }
 }
