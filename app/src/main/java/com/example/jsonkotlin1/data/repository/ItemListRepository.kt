171e4fdc7123901dee1d9570e1846f4a3c2fa51c
Jingyan Sun <jsun2008@163.com>
2022-02-04 15:42:28 -0800

    clean the code

--- app/src/main/java/com/example/jsonkotlin1/data/repository/ItemListRepository.kt
+++ app/src/main/java/com/example/jsonkotlin1/data/repository/ItemListRepository.kt
@@ -1,7 +1,6 @@
 package com.example.jsonkotlin1.data.repository
 
 import androidx.lifecycle.LiveData
-import com.example.jsonkotlin1.data.ItemList
 import com.example.jsonkotlin1.data.db.entity.Item
 
 interface ItemListRepository {
